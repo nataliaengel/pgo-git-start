2d0fdf3cfd5ff55fc07a02b259913b5564486514
nataliaengel <s36411@pjwstk.edu.pl>
2026-03-18 10:01:46 +0100

    Create Czytelnik.java
    nie skonczone

---
+++ src/src/Czytelnik.java
@@ -0,0 +1,8 @@
+public class Czytelnik {
+  private String imie;
+  private String nazwisko;
+  private int numerKarty;
+  private int liczbaWypozyczonych;
+  public Czytelnik(String imie, String nazwisko, int numerKarty) {
+    this.imie = imie;
+    thi
