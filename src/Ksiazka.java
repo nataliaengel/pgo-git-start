91cfb463406f73047e98924833ed5348e63356d0
nataliaengel <s36411@pjwstk.edu.pl>
2026-03-18 09:55:24 +0100

    Update Ksiazka.java

--- src/Ksiazka.java
+++ src/Ksiazka.java
@@ -1,1 +1,16 @@
-
+public class Ksiazka {
+  private String tytul;
+  private String autor;
+  private int liczbaStron;
+  private boolean dostepna;
+  //
+  public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+    this.tytul = tytul;
+    this.autor = autor;
+    this.liczbaStron = liczbaStron;
+    this.dostepna =dostepna;
+  }
+  public void wypiszInfo() {
+    System.out.println(tytul + " - " + autor " (" + liczbaStron + " str.)" );
+  }
+}
