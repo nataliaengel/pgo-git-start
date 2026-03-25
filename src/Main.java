67d7b80def50703a67a2802533d2b98002f6b893
nataliaengel <s36411@pjwstk.edu.pl>
2026-03-25 21:52:23 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,13 @@
-// Autor: Natalia
+public class Main {
+    public static void main(String[] args) {
+        Ksiazka k1 = new Ksiazka("Wiedzmin", "Sapkowski", 300, true);
+        Ksiazka k2 = new Ksiazka("Hobbit", "Tolkien", 250, true);
+        
+        Biblioteka biblioteka = new Biblioteka(10);
+        biblioteka.dodajKsiazke(k1);
+        biblioteka.dodajKsiazke(k2);
+        
+        System.out.println("Zasoby biblioteki:");
+        biblioteka.wypiszDostepneKsiazki();
+    }
+}
