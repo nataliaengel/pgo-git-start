37891b76333114fd206a8ab86c4e6d84bc519f58
nataliaengel <s36411@pjwstk.edu.pl>
2026-04-01 09:29:41 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,19 @@ public class Main {
         
         System.out.println("Zasoby biblioteki:");
         biblioteka.wypiszDostepneKsiazki();
+
+        System.out.println("\n-- Test Czytelnika i wypozyczenia ---");
+        Czytelnik c1 = new Czytelnik("Natalia" , "Engel" , 12345);
+
+        k1.wypozycz(); 
+        c1.zwiekszLiczbeWypozyczonych();
+
+        c1.wypiszDane();
+
+        System.out.println("\n--- Test wyszukiwania ---");
+        biblioteka.znajdzKsiazkePoTytule("Wiedzmin"); 
+
+        int dostepne = biblioteka.policzDostepneKsiazki();
+        System.out.println("Liczba aktualnie dostepnych ksiazek: " + dostepne);
     }
 }
