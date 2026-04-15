a5996a5d8d6cc9599f7ba650aee73616ada32f1f
nataliaengel <s36411@pjwstk.edu.pl>
2026-04-15 10:00:21 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Ksiazka k1 = new Ksiazka("Wiedzmin", "Sapkowski", 300, true);
-        Ksiazka k2 = new Ksiazka("Hobbit", "Tolkien", 250, true);
-        
-        Biblioteka biblioteka = new Biblioteka(10);
-        biblioteka.dodajKsiazke(k1);
-        biblioteka.dodajKsiazke(k2);
-        
-        System.out.println("Zasoby biblioteki:");
-        biblioteka.wypiszDostepneKsiazki();
+        ProduktMenu p1 = new ProduktMenu("K-01" , "Expresso" , 9.0, "kawa");
+        ProduktMenu p2 = new ProduktMenu("K-02", "Cappuccino" , 13.5, "kawa");
+        ProduktMenu p3 = new ProduktMenu("D-01" , "Sernik" , 16.0, "deser");
 
-        System.out.println("\n-- Test Czytelnika i wypozyczenia ---");
-        Czytelnik c1 = new Czytelnik("Natalia" , "Engel" , 12345);
+    KlientKawiarni klient = new KlientKawiarni(100, "Julia", "Mazur" , "j.mazur@mail.pl");
 
-        k1.wypozycz(); 
-        c1.zwiekszLiczbeWypozyczonych();
+    Zamowienie zamowienie = new Zamowienie(klient);
+        zamowienie.dodajProdukt(p1);
+        zamowienie.dodajProdukt(p2);
+        zamowienie.dodajProdukt(p3);
 
-        c1.wypiszDane();
+    System.out.println(klient);
+        System.out.println(zamowienie);
+        System.out.println("Laczna wartosc: " + zamowienie.policzWartosc());
+        System.out.println("Liczba pozycji: " + zamowienie.policzLiczbeProduktow());
+        System.out.println("LiczbaProduktow: " + ProduktMenu.getLiczbaProduktow());
 
-        System.out.println("\n--- Test wyszukiwania ---");
-        biblioteka.znajdzKsiazkePoTytule("Wiedzmin"); 
+    ProduktMenu kopiaEspresso = new ProduktMenu("K-01" , "Expresso duplikat" , 9.0, "kawa");
+        System.out.println("Czy produkty sa rowne? " + p1.equals(kopiaEspresso));
 
-        int dostepne = biblioteka.policzDostepneKsiazki();
-        System.out.println("Liczba aktualnie dostepnych ksiazek: " + dostepne);
+    zamowienie.oznaczJakoOplacone();
+        System.out.println(zamowienie);
     }
 }
