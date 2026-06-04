0e52558d08e3a6df7f42990886a3bdad6f0f9064
nataliaengel <s36411@pjwstk.edu.pl>
2026-06-04 12:57:31 +0200

    Create Zadanie4.java

---
+++ src/Zadanie4.java
@@ -0,0 +1,10 @@
+public class Zadanie4 {
+    public static void main(String[] args) {
+        LibraryAccount account = new LibraryAccount("Jan Nowak", 3, 5);
+        LibraryAccount.FineCalculator calculator = account.new FineCalculator();
+        double fine = calculator.calculate();
+
+        MessagePrinter printer = message -> System.out.println("[BIBLIOTEKA] " + message);
+        printer.print("Czytelnik: " + account.getReaderName() + ", opłata: " + fine + " zł");
+    }
+}
